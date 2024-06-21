85b2683edef8be93b9d5d234d912a2f52ac08e06
victorcynzo <vynzonides@gmail.com>
2024-06-21 11:50:15 +0200

    await

--- keywords_explained.py
+++ keywords_explained.py
@@ -1,9 +1,25 @@
+#await, keyword with async, waits for task and then continues I suppose?
+import asyncio
+from asyncio import Future
+async def my_task(no: int) -> dict:
+    print('Starting task...')
+    await asyncio.sleep(2)
+    return {'task': no}
+
+async def main() -> None:
+    tasks: Future = asyncio. gather(my_task(1), my_task(2), my_task(3))
+    results: list[dict] = await tasks
+    print(results)
+
+if __name__ == '__main__':
+    asyncio.run(main=main())
+
 # async, asynchronised programming, no clue
 import asyncio #asynchronised tasks
 async def main() -> None:
     print("I am an asynchronous function!")
 
-if __name__ == '__keywords_explained__':
+if __name__ == '__main__':
     asyncio.run(main==main())
 
 #assert minimum requirements, in example runs with 'users.db'
